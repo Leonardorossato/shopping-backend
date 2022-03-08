0205014834262b6c0b77cec752d787fa08566d9d
Leonardorossato <leonardorossato21@gmail.com>
2022-03-08 10:10:38 -0300

    import da rota de product para o arquivo principal

--- server.js
+++ server.js
@@ -3,9 +3,12 @@ const app = express()
 const bodyParser = require('body-parser')
 require('dotenv').config()
 const port = process.env.PORT
+const productRouter = require('./routes/productRouter')
 
 app.use(express.json())
 app.use(bodyParser.urlencoded({extended: true}))
+
+app.use('/api/products', productRouter)
 
 app.listen(port, ()=>{
     console.log(`Server is running at port ${port}`)
