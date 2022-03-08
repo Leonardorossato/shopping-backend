7412294ff0fe6cc56512642a5f11abd2f96b518e
Leonardorossato <leonardorossato21@gmail.com>
2022-03-08 10:36:04 -0300

    import do arquivo de conexão com o mongoDb

--- server.js
+++ server.js
@@ -5,12 +5,15 @@ require('dotenv').config()
 const port = process.env.PORT
 const productRouter = require('./routes/productRouter')
 const orderRouder = require('./routes/orderRouder')
+const mongoConnection = require('./connection/mongoConnection')
 
 app.use(express.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
 app.use('/api/products', productRouter)
 app.use('/api/orders', orderRouder)
+
+app.mongoConnection = mongoConnection
 
 app.listen(port, ()=>{
     console.log(`Server is running at port ${port}`)
