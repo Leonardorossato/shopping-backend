4cddc4a16dac107df071a9a72923ca8ab6c8838b
Leonardorossato <leonardorossato21@gmail.com>
2022-03-08 10:36:18 -0300

    conexão com o mongoDB ok

--- connection/mongoConnection.js
+++ connection/mongoConnection.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+require('dotenv').config()
+const mongoURI = process.env.mongoURI
 
 mongoose.connect(mongoURI).then(()=>{
     console.log('Connection with mongoDB was successfully.')
