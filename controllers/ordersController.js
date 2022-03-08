3616b4427f7ac930b15e5e010583538f23a2454a
Leonardorossato <leonardorossato21@gmail.com>
2022-03-08 10:14:11 -0300

    fazendo o controller de order

---
+++ controllers/ordersController.js
@@ -0,0 +1,21 @@
+
+
+class ProductsController{
+    static getAllOrders = async(req, res) => {
+        
+    }
+    static getOrdersById = async(req, res)=>{
+
+    }
+    static createOrder = async(req, res)=>{
+
+    }
+    static updateOrder = async(req, res)=>{
+
+    }
+    static deleteOrder = async(req, res)=>{
+        
+    }
+}
+
+module.exports = ProductsController
