98f9c403f208bd6055849ae4289695c3980ccc69
Leonardorossato <leonardorossato21@gmail.com>
2022-03-08 10:09:58 -0300

    fazendo ainda o controller de product

---
+++ controllers/productsController.js
@@ -0,0 +1,21 @@
+
+
+class ProductsController{
+    static getAllProducts = async(req, res) => {
+        
+    }
+    static getProductsById = async(req, res)=>{
+
+    }
+    static createProduct = async(req, res)=>{
+
+    }
+    static updateProduct = async(req, res)=>{
+
+    }
+    static deleteProduct = async(req, res)=>{
+        
+    }
+}
+
+module.exports = ProductsController
