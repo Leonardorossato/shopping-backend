f7972dba56449c73591a45795a3fa64a818cb069
Leonardorossato <leonardorossato21@gmail.com>
2022-03-08 10:18:09 -0300

    import da rotda de order

--- server.js
+++ server.js
@@ -4,11 +4,13 @@ const bodyParser = require('body-parser')
 require('dotenv').config()
 const port = process.env.PORT
 const productRouter = require('./routes/productRouter')
+const orderRouder = require('./routes/orderRouder')
 
 app.use(express.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
 app.use('/api/products', productRouter)
+app.use('/api/orders', orderRouder)
 
 app.listen(port, ()=>{
     console.log(`Server is running at port ${port}`)
